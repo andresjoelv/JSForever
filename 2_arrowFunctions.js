dca599e1f3c9d19b3aa6aa4c199b976ef2c2aee5
Nejo <andresjoelv@gmail.com>
2019-06-07 17:13:17 -0400

    arrow functions in depth

--- 2_arrowFunctions.js
+++ 2_arrowFunctions.js
@@ -61,3 +61,19 @@ button.addEventListener('click', () => {
     //...
 });
 
+// If no arguments are required, you can substitute the parenthesis (()) for an underscore (_)
+
+// all the followings are valid arrow functions
+const zeroArgs = () => {/* do something */}
+const zeroWithUnderscore = _ => {/* do something */}
+const oneArg = arg1 => {/* do something */}
+const oneArgWithParenthesis = (arg1) => {/* do something */}
+const manyArgs = (arg1, arg2) => {/* do something */}
+
+// Arrow functions, by default, automatically create a return keyword if the code
+// only takes up one line, and is not enclosed in a block
+
+var threeTimes = numbers.map( x => x*3 );
+
+// return is implied if there is an expression after the arrow
+
